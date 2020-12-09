679accff55679c8ba2cfe022a7158a8634a23691
MASTR SMOOV <student@bchd.b1893f81-c119-4b67-9239-8d9b790a5e77>
2020-12-09 20:56:16 +0000

    my first script 2

--- custif/myflix.py
+++ custif/myflix.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
-message = 'This is the SMOOV test'
+message = 'This is the SMOOV test! '
 
 # wrap connection in a float() to accept decimals as numbers
 score = float(input("What is your score?"))
 
-# if input value was higher or equal to 25
+# if input value was higher or equal to 90
 if score >= 90:
-    message = message + ('A')
+    message = ('Oh, you SMOOV SMOOV!')
 elif score >= 80:
-    message = message + ('B')
+    message = ('You SMOOV AF')
 elif score >= 70:
-    message = message + ('C')
+    message = ('You SMOOV')
 else:
     message = message + ('Sorry, you failed the SMOOV test :(')
 print(message)
